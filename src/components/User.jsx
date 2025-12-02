96f868d6c28ac42712a41b3fd22be4999b2ff5c8
Hayat <hayat.moujtahidy@gmail.com>
2025-12-02 10:54:58 +0100

    commit

---
+++ src/components/User.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+
+const User = () => {
+    const[users,setUsers] = useState([])
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(Response => setUsers(Response.data))
+        .catch(error=> console.log("il ya une error")
+    
+        )
+        
+
+        
+
+
+    })
+  return (
+    <div>{Users.map(user => (
+        <h1key={user.id}>{user.name}</h1>
+        ))}
+
+    </div>
+  )
+}
+
+export default User
